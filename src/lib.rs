144a946749974b0fa448546c8d70f84806ecf802
Edward Wenger <wenger.edward@gmail.com>
2025-06-21 21:04:02 -0700

    pass python dict into rust params struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,52 @@ mod core;
 mod polio;
 
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use bevy::prelude::*;
 use bevy::app::AppExit;
 
 use core::{Host, SimulationTime};
 use polio::Immunity;
 
-static N_HOSTS: u32 = 5;       // Number of hosts in the simulation
-static MAX_DAYS: u32 = 60;     // Maximum simulation days
-static INCIDENCE_RATE: f32 = 0.03;  // Per-capita infectious challenge rate per SimulationTime.day
-static LOG10_DOSE: f32 = 6.0;       // Log10 of infectious challenge dose
+#[derive(Resource)]
+#[derive(FromPyObject)]
+struct SimParams {
+    n_hosts: u32,
+    max_days: u32,
+    incidence_rate: f32,
+    log10_dose: f32,
+}
+impl Default for SimParams {
+    fn default() -> Self {
+        Self {
+            n_hosts: 5,
+            max_days: 5,
+            incidence_rate: 0.03,
+            log10_dose: 6.0,
+        }
+    }
+}
 
 /// This function can be called from Python
 #[pyfunction]
-fn run_bevy_app() -> PyResult<()> {
+fn run_bevy_app<'py>(data: &Bound<'py, PyDict>) -> PyResult<()> {
+
+    let mut rust_params_struct = SimParams::default();
+
+    for (key, value) in data.iter() {
+        println!("{}: {} ", key.to_string(), value.to_string());
+        match key.to_string().as_str() {
+            "n_hosts" => rust_params_struct.n_hosts = value.extract::<u32>()?,
+            "max_days" => rust_params_struct.max_days = value.extract::<u32>()?,
+            "incidence_rate" => rust_params_struct.incidence_rate = value.extract::<f32>()?,
+            "log10_dose" => rust_params_struct.log10_dose = value.extract::<f32>()?,
+            _ => println!("Warning: Unknown parameter {}", key.to_string()),
+        }
+    }
+
     App::new()
         .add_plugins(MinimalPlugins)
+        .insert_resource(rust_params_struct)
         .insert_resource(SimulationTime::default())
         .insert_resource(polio::Params::default())
         .add_systems(Startup, setup)
@@ -27,8 +57,11 @@ fn run_bevy_app() -> PyResult<()> {
     Ok(())
 }
 
-fn setup(mut commands: Commands) {
-    for _ in 0..N_HOSTS {
+fn setup(
+    mut commands: Commands,
+    params: Res<SimParams>,
+) {
+    for _ in 0..params.n_hosts {
         commands.spawn((
             Host{birth_sim_day: 0.0},
             Immunity::default(),
@@ -38,9 +71,10 @@ fn setup(mut commands: Commands) {
 
 fn exit_system(
     sim_time: Res<SimulationTime>,
+    params: Res<SimParams>,
     mut exit: EventWriter<AppExit>
 ) {
-    if sim_time.day > MAX_DAYS {
+    if sim_time.day > params.max_days {
         exit.send(AppExit);
     }
 }
@@ -50,6 +84,7 @@ fn step_loop(
     mut host_query: Query<(Entity, &Host, &mut polio::Immunity, Option<&mut polio::Infection>)>,
     mut sim_time: ResMut<SimulationTime>,
     polio_params: Res<polio::Params>,
+    params: Res<SimParams>,
 ) {
         let duration = sim_time.timer.duration();
         sim_time.timer.tick(duration);
@@ -57,8 +92,8 @@ fn step_loop(
         println!("...Advancing to day {}", sim_time.day);
         polio::step_state(&mut commands, &mut host_query, &polio_params, &sim_time);
 
-        let prob = 1.0 - (-INCIDENCE_RATE).exp();
-        let dose = 10f32.powf(LOG10_DOSE);
+        let prob = 1.0 - (-params.incidence_rate).exp();
+        let dose = 10f32.powf(params.log10_dose);
         polio::challenge(&mut commands, &mut host_query, &polio_params, &sim_time, prob, dose);
 }
 
--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 import pybevy as pb
 
-pb.run_bevy_app()
+sim_params = dict(
+    n_hosts=2,
+    max_days=20,
+    incidence_rate=0.2,
+    # log10_dose=8.0,
+)
+
+pb.run_bevy_app(sim_params)
